2c14ccf881d72b364ae1319809eb6208c55f8408
Yousef Shawky <211150997+yousef-shawky-etg@users.noreply.github.com>
2025-09-24 04:53:42 +0300

    fixing bug with google login

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   frontUrl: 'https://joinbbb.org',
   apiUrl: 'https://bbb-partnership-api.playdough.co/api',
-  googleClientId: '512459139525-0e75d380592c314d5701a54220015d56.apps.googleusercontent.com',
+  googleClientId: '1027144688065-hgvpikljl1v51d60m8f9tap887g8s1v7.apps.googleusercontent.com',
   microsoftClientId: '6957306d-123d-4567-8901-234567890123',
 };
